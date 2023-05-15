f1f37ad03de62a690b5babbc39eadfaccbeeabda
Soham2395 <123460167+Soham2395@users.noreply.github.com>
2023-05-15 22:05:01 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var word = "";
 var wordGuess = [];
 var wrongGuess = [];
@@ -9,20 +8,20 @@ var dif = 0;
 
 function chooseDif1() {
     dif = 1;
-    document.getElementById('startButton').style.display='block';
-    document.getElementById('chooseDifficulty').style.display='none';
+    document.getElementById('startButton').style.display = 'block';
+    document.getElementById('chooseDifficulty').style.display = 'none';
 }
 
 function chooseDif2() {
     dif = 2;
-    document.getElementById('startButton').style.display='block';
-    document.getElementById('chooseDifficulty').style.display='none';
+    document.getElementById('startButton').style.display = 'block';
+    document.getElementById('chooseDifficulty').style.display = 'none';
 }
 
 function chooseDif3() {
     dif = 3;
-    document.getElementById('startButton').style.display='block';
-    document.getElementById('chooseDifficulty').style.display='none';
+    document.getElementById('startButton').style.display = 'block';
+    document.getElementById('chooseDifficulty').style.display = 'none';
 }
 
 
@@ -30,7 +29,7 @@ function wordw() {
     var randomWords = ["humor", "miniature", "amusing", "creepy", "fact", "risk", "verse", "land", "lumpy", "holiday", "glorious", "weigh", "brake", "pretty", "grin", "capricious", "bite-sized", "misty", "ignore", "certain", "sloppy", "dress", "true", "zonked", "observation", "action", "various", "want", "direful", "suck", "dress", "scarecrow", "judge", "madly", "quizzical", "consist", "fierce", "love", "arrest", "serve", "fit", "hug", "tan", "curve", "eatable", "tub", "race", "innocent", "open", "preach", "steady", "acoustics", "lock", "field", "arrange", "rifle", "learned", "toe", "flow", "competition", "ill-fated", "oatmeal", "match", "male", "measure", "loaf", "smile", "wrestle", "dull", "food", "locket", "bell", "beg", "strengthen", "responsible", "enchanting", "loutish", "switch", "idea", "nine", "squeamish", "pig", "bat", "dear", "trains", "owe", "frogs", "assorted", "lonely", "hurry", "natural", "sun", "snow", "obnoxious", "broken", "friend", "bright", "cake", "sour", "permit", "economic", "lovely", "quick", "van", "tempt", "apparel", "decay", "business", "adjustment", "blushing", "makeshift", "slippery", "load", "winter", "exist", "tongue", "country", "roll", "fast", "moor", "possess", "pat", "pass", "books", "impartial", "hospitable", "dust", "naughty", "extra-large", "tacky", "produce", "committee", "fuzzy", "judicious", "nebulous", "stick", "ear", "copy", "friendly", "press", "distinct", "vegetable", "upset", "venomous", "statement", "sulky", "spell", "x-ray", "square", "taste", "great", "thumb", "adjoining", "chilly", "test", "ancient", "green", "badge", "work", "repeat", "free", "elderly", "doctor", "difficult", "grubby", "approval", "turn", "vivacious", "thundering", "cherries", "rest", "plan", "crime", "sticks", "wealthy", "phone", "suspend", "gullible", "fence", "note", "wall", "interest", "coil", "jump", "enchanted", "funny", "racial", "greasy", "polish", "elbow", "smart", "bore", "crowd", "glistening", "oval", "eggs", "nauseating", "detailed", "veil", "coal"]
     var raNum = Math.floor(Math.random() * 70);
     return randomWords[raNum]
-    }
+}
 
 
 function wordStart() {
@@ -38,8 +37,8 @@ function wordStart() {
     var wordL_ = "";
     var count = wordLength;
 
-    while(count > 0) {
-        wordGuess.push(" _ ");
+    while (count > 0) {
+        wordGuess.push("<div class='wordBlocks'></div>");
         count -= 1;
     }
 }
@@ -49,12 +48,11 @@ function winCountFunc() {
     var lettUsed = "";
     var count = word.length;
 
-    while(count > 0) {
-        if(lettUsed.includes(word[count - 1])) {
-
+    while (count > 0) {
+        if (lettUsed.includes(word[count - 1])) {
         }
 
-        else{
+        else {
             num += 1;
             lettUsed += word[count - 1];
         }
@@ -69,63 +67,57 @@ function start() {
     word = wordw();
     winCount = winCountFunc();
 
-    if(dif == 1) {
+    if (dif == 1) {
         guessBomb = word.length + 5;
     }
 
-    else if(dif == 2) {
+    else if (dif == 2) {
         guessBomb = word.length;
     }
 
-    else if(dif == 3) {
-        if(word.length % 2 == 0) {
-           guessBomb = word.length / 2;
+    else if (dif == 3) {
+        if (word.length % 2 == 0) {
+            guessBomb = word.length / 2;
         }
-      
+
         else {
-           guessBomb = (word.length - 1) / 2;
+            guessBomb = (word.length - 1) / 2;
         }
     }
 
     console.log(word);
-    document.getElementById('mainGame').style.display='block';
-    document.getElementById('startButton').style.display='none';
-
-    document.getElementById("question").innerHTML = "Enter your first guess";
+    document.getElementById('mainGame').style.display = 'block';
+    document.getElementById('startButton').style.display = 'none';
 
     wordStart();
 
-    document.getElementById('RRguess').style.display='block';
-    document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
+    document.getElementById('RRguess').style.display = 'block';
+    document.getElementById("rightGuess").innerHTML = wordGuess.toString().replaceAll(',', ' ');
     document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
     document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
 
     var x = document.getElementById("guess").maxLength;
-    //document.getElementById("demo").innerHTML = x;
 }
 
 function enterGuess() {
     var lett = document.getElementById("guess").value;
     document.getElementById("guess").value = "";
 
-    if (lett.length === 1){
+    if (lett.length === 1) {
         var rightOnot = isRightOnot(lett);
         if (rightOnot == true) {
-
             NewCW(lett);
         }
 
         else {
-            if(!wrongGuess.includes(lett)) {
-                console.log("hi");
-                wrongGuess.push(lett);
+            if (!wrongGuess.includes(lett)) {
+                wrongGuess.push(`<div class='wrongWordBlocks'>${lett.toString().replaceAll(',', ' ')}</div>`);
             }
             guessBomb -= 1;
         }
     }
 
     else if (lett.length < 1) {
-
     }
 
     else {
@@ -139,7 +131,7 @@ function enterGuess() {
     if (winCount <= 0) {
         gameWin()
     }
-    document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
+    document.getElementById("rightGuess").innerHTML = wordGuess.toString().replaceAll(',', ' ');
     document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
     document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
 }
@@ -155,12 +147,12 @@ function NewCW(letter) {
 
     while (count <= word.length - 1) {
         if (letter === word[count]) {
-            if(wordGuess[count] === letter) {
+            if (wordGuess[count] === letter) {
             }
             else {
             }
 
-            wordGuess[count] = letter;
+            wordGuess[count] = `<div class='wordBlocks'>${letter}</div>`;
             count += 1;
         }
 
@@ -173,24 +165,24 @@ function NewCW(letter) {
 }
 
 function gameLose() {
-    document.getElementById('mainGame').style.display='none';
-    document.getElementById('RRguess').style.display='none';
-    document.getElementById('youLose').style.display='block';
+    document.getElementById('mainGame').style.display = 'none';
+    document.getElementById('RRguess').style.display = 'none';
+    document.getElementById('youLose').style.display = 'block';
     document.getElementById("correctWordWas").innerHTML = "The correct word was " + word;
 }
 
 function gameWin() {
-    document.getElementById('mainGame').style.display='none';
-    document.getElementById('RRguess').style.display='none';
-    document.getElementById('youWin').style.display='block';
+    document.getElementById('mainGame').style.display = 'none';
+    document.getElementById('RRguess').style.display = 'none';
+    document.getElementById('youWin').style.display = 'block';
 }
 
 function restart() {
-    document.getElementById('mainGame').style.display='none';
-    document.getElementById('RRguess').style.display='none';
-    document.getElementById('youLose').style.display='none';
-    document.getElementById('youWin').style.display='none';
-    document.getElementById('chooseDifficulty').style.display='block';
+    document.getElementById('mainGame').style.display = 'none';
+    document.getElementById('RRguess').style.display = 'none';
+    document.getElementById('youLose').style.display = 'none';
+    document.getElementById('youWin').style.display = 'none';
+    document.getElementById('chooseDifficulty').style.display = 'block';
 
     word = "";
     wordGuess = [];
